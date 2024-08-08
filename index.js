d296b47e4f5d9876dfc090e24388fd76b0a38a12
Tushar Panchal <tusharpanchal0761@gmail.com>
2024-08-09 01:29:05 +0530

    Completed

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-console.clear()
+console.clear();
+const readline = require("readline");
 const simpleGit = require("simple-git");
 const createCsvWriter = require("csv-writer").createObjectCsvWriter;
 
 const git = simpleGit();
-
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
 const csvWriter = createCsvWriter({
     path: "commit_stats.csv",
     header: [
@@ -16,71 +20,87 @@ const csvWriter = createCsvWriter({
     ],
 });
 
+const findAllAuthor = (commits) =>
+    new Set(commits.map((commit) => commit.author_name));
+
+const promptForAuthor = (authorsName) => {
+    console.log("---  Here is all the authors List...");
+
+    authorsName.forEach((author, index) => console.log(index + 1, author));
+
+    return new Promise((resolve) => {
+        const ask = () => {
+            rl.question("Which author do you want to search?\n", (input) => {
+                const regex = new RegExp(input, "i");
+                const matchedAuthor = authorsName.find((author) =>
+                    regex.test(author)
+                );
+
+                if (matchedAuthor) {
+                    console.log(`---- Analyzing author: ${matchedAuthor} ----`);
+                    resolve(matchedAuthor);
+                } else {
+                    console.log(
+                        "No author found with the name you provided. Please try again."
+                    );
+                    ask(); // Recursively prompt again if no match is found
+                }
+            });
+        };
+        ask();
+    });
+};
+
+const prepareCommitData = async (commits) => {
+    let rowData = [];
+    let count = 0;
+
+    for (let i = 1; i < commits.length; i++) {
+        // Start from the second commit to compare with the previous one
+        let { message: message1, hash: hash1, date: date1 } = commits[i];
+        let { message: message2, hash: hash2, date: date2 } = commits[i - 1];
+
+        date1 = new Date(date1).toLocaleString(); // current
+        date2 = new Date(date2).toLocaleString(); // previous
+
+        const diff = await git.diffSummary([hash2, hash1]);
+        const { insertions, deletions } = diff;
+
+        rowData.push({
+            count: ++count,
+            message: message1,
+            date: date1,
+            addedChars: insertions,
+            removedLines: deletions,
+            hash: hash1,
+        });
+    }
+    return rowData;
+};
+
 async function getCommitStats() {
     try {
+        const log = await git.log();
 
-      const log = await git.log();
-console.log(log);
+        let commits = log.all.reverse();
+        
+        const authorsName = [...findAllAuthor(commits)];
 
-      let commits = log.all;
+        const matchedAuthor = await promptForAuthor(authorsName);
 
-        commits = commits.reverse();
-        commits = commits.filter((c) => c.author_name === "Tushar Panchal");
-      const rowData = []
-      // console.log(commits[0]);
+        commits = commits.filter((c) => c.author_name === matchedAuthor);
 
-        let count = 0;
-        console.log("Loop Started");
+        const commitData = prepareCommitData(commits);
 
-        for (const commit in commits) {
-          if(commit==0 )          {
-            continue
-          }
-          // if(commit>5){
-          //   break
-          // }
-          let { message:message1, hash:hash1, date:date1, } = commits[commit];
-          let {   message:message2, hash:hash2,date:date2 } = commits[commit-1];
-
-          date1 = new Date(date1).toLocaleString() // current 
-          date2 = new Date(date2).toLocaleString() // previous
-
-            let msg = "";
-            count += 1;
-            const diff = await git.diffSummary([hash2, hash1]);
-            msg += `----------------------------   ${count}   ------------------------------\n`;
-            // date = date.slice(0, 10);
-      // console.log(diff);
-
-            const { insertions, deletions } = diff;
-            msg += "\tmessage cur| " + message1 + "\n";
-            msg += "\tmessage prev| " + message2 + "\n";
-            // msg += "\thash \t| " + hash + "\n";
-            msg += "\tdate cur\t| " + date1 + "\n";
-            msg += "\tdate prev\t| " + date2 + "\n";
-            msg += "\tinsertion | " + insertions + "\n";
-            msg += "\tdeletions | " + deletions + "\n";
-
-            rowData.push({
-              count,
-              message:message1, date:date1,addedChars:insertions,removedLines:deletions,hash:hash1
-            })
-
-          }
-          console.log("loop done");
-          console.timeEnd("loop done");
-          console.time("Write in CSV");
-          
-        await csvWriter.writeRecords(rowData);
+        console.time("Write in CSV");
+        await csvWriter.writeRecords(commitData);
         console.timeEnd("Write in CSV");
-        console.timeEnd("Function Started");
-
-        console.log('CSV file created successfully.');
+        console.log("CSV file created successfully.");
+        rl.close()
     } catch (err) {
-      console.log("error out");
+        console.log("error out", err);
+        rl.close()
         // console.error('Error generating commit stats:', err);
     }
-
 }
-
 getCommitStats();
